22479a805292f5eadfeefd609d6bf16940203f0f
paraqles <paraqles@dthal.net>
2014-08-27 13:39:29 +0200

    Changed ssh-agent starting behaviour to make it more resistent.

--- agent.zsh
+++ agent.zsh
@@ -1,41 +1,53 @@
+export SSH_AUTH_SOCK=$HOME/.ssh/auth_socket
+
 if [ -z "$SSH_CONNECTION" ]; then
-  if [ ${SSH_AUTH_SOCK[(i)gpg]} ]; then
+  gpg=${SSH_AUTH_SOCK#gpg}
+  if [ -n "$gpg" -a "$gpg" != "$SSH_AUTH_SOCK" ]; then
     unset SSH_AGENT_PID
     unset SSH_AUTH_SOCK
   fi
   if [ -z "$SSH_AGENT_PID" ]; then
     if [ -x $(which pgrep) ]; then
       SSH_AGENT_PID=$(pgrep ssh-agent)
-      export SSH_AGENT_PID
     else
-      SSH_AGENT_PID=$(ps -u `whoami` -xco pid,command | grep -i ssh-agent | tr -s " " | sed -e 's/^ *//g' -e 's/ *$//g' | cut -d " " -f 1)
-      export SSH_AGENT_PID
+      SSH_AGENT_PID=$(ps -u `whoami` -xco pid,command | grep -i ssh-agent | sed -E -e 's/^\s*//g' -e 's/\s{2,}/ /g' | cut -d " " -f 1)
     fi
+    export SSH_AGENT_PID
 
     if [ -z "$SSH_AGENT_PID" ]; then
-      SSH_AUTH_SOCK=$HOME/.ssh/auth_socket
-      if [ -r "$SSH_AUTH_SOCK" ]; then
-        rm "$SSH_AUTH_SOCK"
+      if [ -w "$SSH_AUTH_SOCK" ]; then
+        rm $SSH_AUTH_SOCK
       fi
-      eval $(ssh-agent -a $SSH_AUTH_SOCK -s)
+        
+      eval `ssh-agent -a $SSH_AUTH_SOCK -s`
     fi
   fi
-  if [ -z "$SSH_AUTH_SOCK" ]; then
-    export SSH_AUTH_SOCK=$HOME/.ssh/auth_socket
-  fi
+
+  export SSH_AUTH_SOCK
+  export SSH_AGENT_PID
 
   SSH_ADD=$(which ssh-add)
   keys=$($SSH_ADD -l | cut -d " " -f 3)
 
   add_key=()
   for k_file in `ls $SSH_ID_DIR/*`; do
-    if [ ! ${keys[(i)$k_file]} -le ${#keys} ]; then
-      add_key[$(($#add_key +1))]=$k_file
+    pub=${k_file%pub}
+    ppk=${k_file%ppk}
+
+    # If $pub and $ppk equal to $k_file, the file name does not contain
+    # pub or ppk at most end.
+    # So then it is save to try to add this id file to the agent.
+    if [ "$pub" = "$k_file" -a "$ppk" = "$k_file" ]; then
+      if [ ! ${keys[(i)$k_file]} -le ${#keys} ]; then
+        add_key[$(($#add_key +1))]=$k_file
+      fi
     fi
   done
 
   if [[ ! -z "$add_key" ]]; then
     $SSH_ADD $add_key
   fi
+
+  unset SSH_ADD
 fi
 
