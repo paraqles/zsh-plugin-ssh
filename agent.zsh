f6d5ad27846f4f90571bd5ecb2fb06ab9444ded8
paraqles <paraqles@dthal.net>
2014-12-26 00:40:15 +0100

    Added test for mac os launchd agent.

--- agent.zsh
+++ agent.zsh
@@ -13,37 +13,41 @@ if [ -z "$SSH_CONNECTION" ]; then
     unset SSH_AUTH_SOCK
   fi
 
-  # Check if we have a PID for an ssh-agent
-  if [ -z "$SSH_AGENT_PID" ]; then
+  if [[ ! $SSH_AUTH_SOCK =~ "com\.apple\.launchd" ]]; then
 
-    # If we have not try to find if a agent is running for the current user
-    if [ -x $(which pgrep) ]; then
-      SSH_AGENT_PID=$(pgrep -u `whoami` ssh-agent)
+    # Check if we have a PID for an ssh-agent
+    if [[ -z "$SSH_AGENT_PID" ]]; then
+
+      # If we have not try to find if a agent is running for the current user
+      if [[ -x $(which pgrep) ]]; then
+        echo "Find SSH Agent with pgrep"
+        SSH_AGENT_PID=$(pgrep -u `whoami` ssh-agent)
+      else
+        echo "Find SSH Agent with ps"
+        SSH_AGENT_PID=$(ps -u `whoami` -o pid,command | grep -i sh-ag | grep -i ent | sed -Ee 's/^\s*//g' | cut -d " " -f 1)
+      fi
+      export SSH_AGENT_PID
+
     else
-      SSH_AGENT_PID=$(ps -u `whoami` -o pid,command | grep -i sh-ag | grep -i ent | sed -Ee 's/^\s*//g' | cut -d " " -f 1)
-    fi
-    export SSH_AGENT_PID
+      # if we have a PID, check if the corresponding agent is still running
+      if [[ ! `kill -0 $SSH_AGENT_PID 2>/dev/null` ]]; then
+        unset SSH_AGENT_PID
+      fi
 
-  else
-    # if we have a PID, check if the corresponding agent is still running
-    if kill -0 $SSH_AGENT_PID 2>/dev/null; then
-      # Just a NOP (do not know better way)
-      echo "" > /dev/null
-    else
-      unset SSH_AGENT_PID
-    fi
-  fi
-
-  # if we have no PID start a new agent.
-  if [ -z "$SSH_AGENT_PID" ]; then
-
-    SSH_AUTH_SOCK=$HOME/.ssh/auth_socket
-    # check if there is an old socket file and remove it
-    if [ -w "$SSH_AUTH_SOCK" ]; then
-      rm $SSH_AUTH_SOCK
     fi
 
-    eval `ssh-agent -a $SSH_AUTH_SOCK -s`
+    # if we have no PID start a new agent.
+    if [[ -z "$SSH_AGENT_PID" ]]; then
+
+      SSH_AUTH_SOCK=$HOME/.ssh/auth_socket
+      # check if there is an old socket file and remove it
+      if [ -w "$SSH_AUTH_SOCK" ]; then
+        rm $SSH_AUTH_SOCK
+      fi
+
+      eval `ssh-agent -a $SSH_AUTH_SOCK -s`
+    fi
+
   fi
 
   # Set environment
@@ -62,18 +66,8 @@ if [ -z "$SSH_CONNECTION" ]; then
     # go through all files in $SSH_ID_DIR
     add_key=()
     for k_file in `ls $SSH_ID_DIR/*`; do
-      #pub=${k_file%pub}
-      #ppk=${k_file%ppk}
-
-      # If $pub and $ppk equal to $k_file, the file name does not contain
-      # pub or ppk at most end.
-      # So then it is save to try to add this id file to the agent.
-      #if [ "$pub" = "$k_file" -a "$ppk" = "$k_file" ]; then
-
       if [[ "$k_file" -pcre-match 'id_[a-zA-Z0-9]*_?rsa(?!\.(pub)|(ppk))$' ]]; then
         # Check if current key is already in the ssh-agent.
-        #if [ ! ${keys[(i)$k_file]} -le ${#keys} ]; then
-
         if [[ ! $keys =~ $k_file ]]; then
           add_key[$(($#add_key +1))]=$k_file
         fi
